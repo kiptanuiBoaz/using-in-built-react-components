26ffba2a6cbed171f41671821a437b59a3fa99a5
boaz <boaserem022@gmail.com>
2022-06-15 18:32:37 +0300

    third commit

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import Fab from "@mui/material/Fab";
+import Zoom from '@mui/material/Zoom';
 
 function CreateArea(props) {
   const [note, setNote] = useState({
@@ -28,6 +31,7 @@ function CreateArea(props) {
 
   return (
     <div>
+
       <form className="create-note">
         <input
           name="title"
@@ -42,8 +46,12 @@ function CreateArea(props) {
           placeholder="Take a note..."
           rows="3"
         />
-        <button onClick={submitNote}>Add</button>
+
+        <Zoom in= {true}>
+          <Fab onClick={submitNote}>< AddIcon/></Fab>
+        </Zoom>
       </form>
+   
     </div>
   );
 }
